fca401592b62c12606a6003a7a3dbe14cc73eee4
Chialvo <lautarochialvo2005@gmail.com>
2024-08-30 11:12:34 -0300

    fix3

--- olimpiada/config/settings.py
+++ olimpiada/config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'olimpiada.config.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'olimpiada.config.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
