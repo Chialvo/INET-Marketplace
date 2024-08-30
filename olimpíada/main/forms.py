2ad2d309c7429e1d62c749923985501f4f22f60c
david <davidandradag@gmail.com>
2024-08-30 09:09:06 -0300

    hecho register

--- olimpíada/main/forms.py
+++ olimpíada/main/forms.py
@@ -14,3 +14,28 @@ class PedidoProductoForm(ModelForm):
     class Meta:
         model = PedidoProducto
         fields = ['producto','cantidad']
+        
+class ClienteRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Cliente
+        fields = ['nombre', 'direccion', 'telefono', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords don't match")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        cliente = super().save(commit=False)
+        cliente.set_password(self.cleaned_data['password'])
+        if commit:
+            cliente.save()
+        return cliente
