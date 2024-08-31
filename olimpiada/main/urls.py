4628a5dfba69f9aaabb35e9bc7e68bea24a0909b
Chialvo <lautarochialvo2005@gmail.com>
2024-08-31 16:35:11 -0300

    cleaning

--- olimpiada/main/urls.py
+++ olimpiada/main/urls.py
@@ -12,11 +12,11 @@ router.register('clientes', ClienteViewSet, 'clientes')
 urlpatterns = [
     path('', home, name='home'),
     path('productos/', productos, name='productos'),
-    path('login/', login_view, name='login'),  # Ensure this is correct
+    path('login/', login_view, name='login'),
     path('register/', register, name='register'),
     path('pedido/', pedido, name='pedido'),
     path('agregarAlCarrito/<int:id>/', agregarAlCarrito, name='agregarAlCarrito'),
-    path('login_cliente/', login_cliente, name='login_cliente'),  # Ensure this is correct
+    path('login_cliente/', login_cliente, name='login_cliente'),
     path('mostrarPedidos/', mostrarPedidos, name='mostrarPedidos'),
     path('crear_pedido/', crear_pedido, name='crear_pedido'),
     path('register/', register, name='register'),    
